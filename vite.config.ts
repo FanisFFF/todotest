cc98f23f6537f25b98403665686203c497a15ab8
FanisFFF <faskhutdinovfanis@gmail.com>
2025-07-08 13:59:51 +0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),
-  ], test: {
-    environment: 'jsdom',        // Use browser-like environment
-    globals: true,               // Enable global test APIs like describe/it
-    setupFiles: './src/setupTests.ts', // Run setup file before tests
-    include: ['src/**/*.{test,spec}.{ts,tsx}'], // Test file patterns
+  plugins: [react(), tailwindcss()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.ts',
+    include: ['src/**/*.{test,spec}.{ts,tsx}'],
   },
-
 })
